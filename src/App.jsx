363ab91b2951fbb70aa0ceace50c9fc48009c3dc
pandaka87 <noellie.peuch2@gmail.com>
2022-12-05 16:01:53 +0100

    Add BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-
+import Home from "./pages/Home";
+import About from "./pages/About";
+import PrivateRoute from "./config/PrivateRoute";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Resetpassword from "./pages/ResetPassword";
+import Profile from "./pages/Profile";
+import EditProfile from "./pages/EditProfile";
+import Footer from "./components/Footer";
 import './App.css'
 
 function App() {
 
   return (
+    <BrowserRouter>
       <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/resetpassword" element={<Resetpassword />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/editprofile" element={<EditProfile />} />
+          </Route>
+        </Routes>
+      </main>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
