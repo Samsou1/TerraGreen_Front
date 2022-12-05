6412d52e8e9be981ffaa1c922f2d414a75773d67
Gregory <gregory@gregory.home>
2022-12-05 15:49:13 +0100

    Import navbar to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 import './App.css'
 
 function App() {
 
   return (
-    <div className="App">
-      
-    </div>
+      <Navbar />
   )
 }
 
