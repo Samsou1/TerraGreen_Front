403582892cb9d64c543e9ee057d49470c8630073
pandaka87 <noellie.peuch2@gmail.com>
2022-12-05 16:12:53 +0100

    Add HomePage + components Hero & footer

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,13 @@
+import Cookies from 'js-cookie';
+import { Link } from "react-router-dom";
+import videoBg from "../../videos/videoBg.mp4";
+
+const Hero = () => {
+  return (
+    <>
+      <h2>Hero</h2>
+    </>
+  )
+}
+
+export default Hero;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import Hero from '../../components/Hero';
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <h2>Home</h2>
+    </>
+  )
+}
+
+export default Home;
