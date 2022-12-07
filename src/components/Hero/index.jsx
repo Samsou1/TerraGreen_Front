414a2b6434fbbd68d6e6e9fd702bb5651236921c
ChrisPfeiffer <chris.cp147@gmail.com>
2022-12-07 14:29:38 +0100

    next modif hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,21 +1,33 @@
-//import Cookies from 'js-cookie';
+import Cookies from 'js-cookie';
 import { Link } from "react-router-dom";
 import videoBg from "../../videos/videoBg.mp4";
 
 const Hero = () => {
-
-  return (
-    // autoPlay loop (Add it later before muted)
-    <section className="hero-container">
-      <video src={videoBg} muted></video>
-      <div>
-        <h1>Bienvenue sur TerraGreen</h1>
-        <p>Participez à sauver notre planète !</p>
-        <Link to="/register">S'inscrire</Link>
-      </div>
-    </section>
-    );
+  if (Cookies.get('currentUser')) {
+    //return (
+      // autoPlay loop (Add it later before muted)
+      <section className="hero-container">
+        <video src={videoBg} muted></video>
+        <div>
+          <h1>Bienvenue sur TerraGreen</h1>
+          <p>Participez à sauver la planète !</p>
+          <Link to="/myprojects">Mes projets</Link>
+        </div>
+      </section>
+      //);
+    //} else {
+      return (
+        // autoPlay loop (Add it later before muted)
+        <section className="hero-container">
+          <video src={videoBg} muted></video>
+          <div>
+            <h1>Bienvenue sur TerraGreen</h1>
+            <p>Participez à sauver la planète !</p>
+            <Link to="/register">S'inscrire</Link>
+          </div>
+        </section>
+      );
+    };
+  }
   
-  };
-
-export default Hero;
+  export default Hero;
