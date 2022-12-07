bf2e20a0585f89f33d7affc06ebf060d7eb2f199
ChrisPfeiffer <chris.cp147@gmail.com>
2022-12-07 13:44:04 +0100

    first commit hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,12 +1,21 @@
-import Cookies from 'js-cookie';
+//import Cookies from 'js-cookie';
 import { Link } from "react-router-dom";
+import videoBg from "../../videos/videoBg.mp4";
 
 const Hero = () => {
+
   return (
-    <>
-      <h2>Hero</h2>
-    </>
-  )
-}
+    // autoPlay loop (Add it later before muted)
+    <section className="hero-container">
+      <video src={videoBg} muted></video>
+      <div>
+        <h1>Bienvenue sur TerraGreen</h1>
+        <p>Participez à sauver notre planète !</p>
+        <Link to="/register">S'inscrire</Link>
+      </div>
+    </section>
+    );
+  
+  };
 
 export default Hero;
