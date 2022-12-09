39b013c49779fc40fe4ece2c3a9bcbeb6fa2112b
Samsou1 <sami.linard.pro@gmail.com>
2022-12-09 19:29:17 +0100

    change the right production back url

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import Cookies from "js-cookie";
 let BASE_URL;
 switch (process.env.NODE_ENV) {
   case "production":
-    url = "https://stackoverflow.com";
+    url = "https://terra-green.fly.dev/";
     break;
   case "development":
     BASE_URL = "http://localhost:3000";
