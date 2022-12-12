34d1e2be0c2e0fc453e31d3eb1c545b1fa6657e4
pandaka87 <noellie.peuch2@gmail.com>
2022-12-12 10:33:02 +0100

    WIP projectCard

--- src/components/ProjectCards/ProjectCard/index.jsx
+++ src/components/ProjectCards/ProjectCard/index.jsx
@@ -6,52 +6,26 @@ import { getProjectStatusFromId } from "../../../services/turnIdToString";
 const ProjectCard = ({ project }) => {
   return (
     <div className="projectCard">
-      <div className="product-details">
-        <h3>{project.attributes.title}</h3>
-        <li>{project.attributes.content}</li>
-        <li>
-          <strong>Status: </strong>
-          {getProjectStatusFromId(project.attributes.project_status_id).name}
-        </li>
-        <li>
-          <strong>Address: </strong>
-          {project.attributes.address}
-        </li>
-        <li>
-          <strong>ZIP: </strong>
-          {project.attributes.postal_code}{" "}
-        </li>
-        <li>
-          <strong>City: </strong>
-          {project.attributes.city}
-        </li>
-        <li>
-          <strong>Postal code: </strong>
-          {project.attributes.postal_code}
-        </li>
-        <li>
-          <strong>Region: </strong>
-          {getRegionFromId(project.attributes.region_id).name}
-        </li>
-        <li>
-          <strong>Country: </strong>
-          {getCountryFromId(project.attributes.country_id).name}
-        </li>
-        <li>
-          <strong>GPS: </strong>
-          {project.attributes.GPS}
-        </li>
-      </div>
-      <div className="control">
-        <button className="btn">
-          <span className="look">
-            <Link className="link" to={`/projects/${project.id}`}>
+      {/* <img src={project.attributes.image_url} /> */}
+      <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
+      <div class="card__overlay">
+        <div class="card__header">
+          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
+          <img class="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" />
+          <div class="card__header-text">
+            <h3 class="card__title">{project.attributes.title}</h3>
+            <span class="card__tagline">{project.attributes.city} {project.attributes.postal_code}</span>
+            <span class="card__tagline">{getRegionFromId(project.attributes.region_id).name}, {getCountryFromId(project.attributes.country_id).name}</span>            
+            <span class="card__status">Username</span>
+          </div>
+        </div>
+        <p class="card__description">{project.attributes.content}</p>
+        <button className="projectBtn">
+            <Link to={`/projects/${project.id}`}>
               Have a look
             </Link>
-          </span>
         </button>
       </div>
-      <img src={project.attributes.image_url} />
       {console.log(project)}
     </div>
   );
