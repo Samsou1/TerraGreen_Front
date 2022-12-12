c4cf8294aae4f4e4a2a9ad457b370c3401fb4cb0
pandaka87 <noellie.peuch2@gmail.com>
2022-12-12 11:12:29 +0100

    Change class in className

--- src/components/ProjectCards/ProjectCard/index.jsx
+++ src/components/ProjectCards/ProjectCard/index.jsx
@@ -7,18 +7,18 @@ const ProjectCard = ({ project }) => {
   return (
     <div className="projectCard">
       {/* <img src={project.attributes.image_url} /> */}
-      <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
-      <div class="card__overlay">
-        <div class="card__header">                     
-          <img class="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" />
-          <div class="card__header-text">
-            <h3 class="card__title">{project.attributes.title}</h3>
-            <span class="card__tagline">{project.attributes.city} {project.attributes.postal_code}</span>
-            <span class="card__tagline">{getRegionFromId(project.attributes.region_id).name}, {getCountryFromId(project.attributes.country_id).name}</span>            
-            <span class="card__status">Username</span>
+      <img src="https://images.unsplash.com/photo-1562077981-4d7eafd44932?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" class="card__image" alt="project illustration" />
+      <div className="card__overlay">
+        <div className="card__header">                     
+          <img className="card__thumb" src="https://images.unsplash.com/photo-1557053910-d9eadeed1c58?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="user avatar" />
+          <div className="card__header-text">
+            <h3 className="card__title">{project.attributes.title}</h3>
+            <span className="card__tagline">{project.attributes.city} {project.attributes.postal_code}</span>
+            <span className="card__tagline">{getRegionFromId(project.attributes.region_id).name}, {getCountryFromId(project.attributes.country_id).name}</span>            
+            <span className="card__status">Username</span>
           </div>
         </div>
-        <p class="card__description">{project.attributes.content}</p>
+        <p className="card__description">{project.attributes.content}</p>
         <button className="projectBtn">
             <Link to={`/projects/${project.id}`}>
               Have a look
