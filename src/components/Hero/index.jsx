16c4edd24315c8d07047e118b636ba129052fb2f
Samsou1 <sami.linard.pro@gmail.com>
2022-12-15 12:12:53 +0100

    commit merge main

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -11,10 +11,9 @@ const Hero = () => {
         <div>
           <h1>Bienvenue sur TerraGreen</h1>
           <p>Créer ton projet TerraGreen</p>
-           <Link to="/newproject">New Project</Link>
+          <Link to="/newproject">New Project</Link>
         </div>
       </section>
-<<<<<<< HEAD
     );
     //);
   } else {
@@ -27,27 +26,9 @@ const Hero = () => {
           <p>Participez à sauver la planète !</p>
           <Link to="/register">S'inscrire</Link>
         </div>
+        <hr />
       </section>
     );
-=======
-    )
-      //);
-    } else {
-      return (
-        // autoPlay loop (Add it later before muted)
-        <section className="hero-container">
-          <video src={videoBg} muted></video>
-          <div>
-            <h1>Bienvenue sur TerraGreen</h1>
-            <p>Participez à sauver la planète !</p>
-            <Link to="/register">S'inscrire</Link>
-          </div>
-          <hr/>
-        </section>
-        
-      );
-    };
->>>>>>> e20bac5f21098ab5889c0848519c0d7d0d8c8583
   }
 };
 
--- src/pages/ShowProject/index.jsx
+++ src/pages/ShowProject/index.jsx
@@ -28,12 +28,8 @@ const ShowProject = () => {
     setProject(data.project);
     setLikes(data.likes);
     setComments(data.comments);
-<<<<<<< HEAD
-    setProjectRegistrations(data.project_registrations);
-=======
     setProjectRegistrations(data.project_registrations)
     setImage(data.image_url)
->>>>>>> e20bac5f21098ab5889c0848519c0d7d0d8c8583
   };
 
   useEffect(() => {
@@ -69,40 +65,6 @@ const ShowProject = () => {
               <p>{project.status}</p>
             </div>
           </div>
-<<<<<<< HEAD
-          <div className="projectInfos">
-            <div className="projectDescription">
-              <h1>{project.title}</h1>
-              <h2>Username</h2>
-              <p>{project.content}</p>
-              <div className="projectLocation">
-                <h3>Location</h3>
-                <p>{project.address}</p>
-                <p>
-                  {project.city} {project.postal_code}
-                </p>
-                <p>
-                  {region}, {country}
-                </p>
-              </div>
-              <div className="projectStatus">
-                <h3>Status</h3>
-                <p>{project.status}</p>
-              </div>
-            </div>
-            <div className="projectBtnShow">
-              <Like likes={likes} />
-              <Link className="edit" to={`/editproject/${project.id}`}>
-                Edit
-              </Link>
-              <DeleteProjectButton className="edit_btn" />
-            </div>
-          </div>
-        </div>
-        <button onClick={() => navigate(-1)}>Go back</button>
-        <CommentsContainer comments={comments} />
-      </div>
-=======
           <div className="projectBtnShow">
             <Like likes={likes} />
             <Link className="editBtn" to={`/editproject/${project.id}`}>Edit</Link>
@@ -116,21 +78,13 @@ const ShowProject = () => {
         <Mapping project={project}/>
       </div>
     </div>
->>>>>>> e20bac5f21098ab5889c0848519c0d7d0d8c8583
     );
   } else {
     return (
       <div className="showContainer">
         <div className="projectWrapper">
           <div className="projectImg">
-<<<<<<< HEAD
-            <img
-              src="https://images.unsplash.com/photo-1562077981-4d7eafd44932?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-              alt="project illustration"
-            />
-=======
           <img src={image} alt="project illustration" />
->>>>>>> e20bac5f21098ab5889c0848519c0d7d0d8c8583
           </div>
           <div className="projectInfos">
             <div className="projectDescription">
