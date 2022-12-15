9f33fa753728ce39077e1fa8d277ace6a2a86792
Samsou1 <sami.linard.pro@gmail.com>
2022-12-15 12:23:10 +0100

    Forbid injection through search bar

--- src/components/Map/Mapping.jsx
+++ src/components/Map/Mapping.jsx
@@ -1,61 +1,49 @@
-import React, { useState, useEffect } from 'react'
-import Map from './index';
+import React, { useState, useEffect } from "react";
+import Map from "./index";
 
-
-  function Mapping({project})  {
-  const [coords, setCoords] = useState ({
+function Mapping({ project }) {
+  const [coords, setCoords] = useState({
     latitude: "",
-    longitude: ""
-  
+    longitude: "",
   });
-  const name = project.title
+  const name = project.title;
 
-
-
-
-
-useEffect(() => {
-  let url = `https://nominatim.openstreetmap.org/search?
+  useEffect(() => {
+    let url = `https://nominatim.openstreetmap.org/search?
   street=${project.address}
   &city=${project.city}
   &postalcode=${project.postal_code}&format=json`;
-  getData(url)
-}, [project]);
+    getData(url);
+  }, [project]);
 
-
-
-
-
-function getData(url) {
-  fetch(url, {
-    method: "POST",
-    mode: "cors",
-    headers: {
-      "Access-Control-Allow-Origin": "https://o2cj2q.csb.app"
-    }
-  })
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-      
+  function getData(url) {
+    fetch(url, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Access-Control-Allow-Origin": "https://o2cj2q.csb.app",
+      },
     })
-    .then((data) => {
-      setCoords({
-        latitude: data[0].lat,
-        longitude: data[0].lon
-      });
-    })
-    
-    .catch(() => error("Please Check your input"));
-}
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        setCoords({
+          latitude: data[0].lat,
+          longitude: data[0].lon,
+        });
+      })
+
+      .catch(() => error("Please Check your input"));
+  }
 
   return (
     <>
-    <Map coords={coords} name={name}   />
-    
+      <Map coords={coords} name={name} />
     </>
-  )
+  );
 }
 
-export default Mapping
+export default Mapping;
