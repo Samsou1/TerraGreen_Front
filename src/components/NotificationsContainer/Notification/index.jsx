41af73b917017a078317bc51f73759fbffd106cb
Samsou1 <sami.linard.pro@gmail.com>
2022-12-16 15:44:54 +0100

    add link to notifications

--- src/components/NotificationsContainer/Notification/index.jsx
+++ src/components/NotificationsContainer/Notification/index.jsx
@@ -1,6 +1,17 @@
+import { Link } from "react-router-dom";
 import { convertTimeStampToDate } from "../../../services/convertTimeStamp";
 
 const Notification = ({ notification }) => {
+
+  if(notification.project_id){
+    return (
+      <div className="notification">
+        <p>Received on {convertTimeStampToDate(notification.created_at)}</p>
+        <p>{notification.content}</p>
+        <Link to={`/projects/${notification.project_id}`}>Click here</Link>
+      </div>
+    );
+  }
   return (
     <div className="notification">
       <p>Received on {convertTimeStampToDate(notification.created_at)}</p>
