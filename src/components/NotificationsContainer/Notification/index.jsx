2a081d843ef5e8346aa8e66c9725d02f59de2912
Samsou1 <sami.linard.pro@gmail.com>
2022-12-16 16:10:25 +0100

    change in hero and home

--- src/components/NotificationsContainer/Notification/index.jsx
+++ src/components/NotificationsContainer/Notification/index.jsx
@@ -2,8 +2,7 @@ import { Link } from "react-router-dom";
 import { convertTimeStampToDate } from "../../../services/convertTimeStamp";
 
 const Notification = ({ notification }) => {
-
-  if(notification.project_id){
+  if (notification.project_id) {
     return (
       <div className="notification">
         <p>Received on {convertTimeStampToDate(notification.created_at)}</p>
