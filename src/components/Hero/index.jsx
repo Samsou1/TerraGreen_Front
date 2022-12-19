2a47ff9240351ab6fcba3e19d20c3b9b9f82fbba
ChrisPfeiffer <chris.cp147@gmail.com>
2022-12-19 13:05:07 +0100

    Video home play

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     return (
       // autoPlay loop (Add it later before muted)
       <section className="hero-container">
-        <video src={videoBg}   muted></video>
+        <video src={videoBg}  autoPlay loop muted></video>
         <div>
           <h1>Welcome to TerraGreen</h1>
           <p>Create your cleaning project with TerraGreen</p>
@@ -20,7 +20,7 @@ const Hero = () => {
       return (
         // autoPlay loop (Add it later before muted)
         <section className="hero-container">
-          <video src={videoBg}  loop></video>
+          <video src={videoBg} autoPlay loop muted></video>
           <div>
             <h1>Welcome to TerraGreen</h1>
             <p>Participate to save Earth!</p>
