724587dd3fed5876c6f322bff1a89299c2c55023
PierreAND <andrieu@gmail.com>
2022-12-19 10:07:23 +0100

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -45,6 +45,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/*" element={<NotFound />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/projects/:id" element={<ShowProject />} />
           <Route element={<PrivateRoute />}>
